ce11423d73706e316a7d2f2afdbb7877b5704627
keisuzuk-rush <84221784+keisuzuk-rush@users.noreply.github.com>
2021-09-03 12:41:08 +0900

    userモデルにバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	with_options presence: true do
+		validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze }
+		validates :company_number, format: { with: /\A[0-9]{8}+\z/.freeze }
+	end
+	validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
