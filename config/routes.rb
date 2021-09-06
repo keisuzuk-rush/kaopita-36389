ec3ac977db43b1186b08d39957db464fb38962ec
keisuzuk-rush <84221784+keisuzuk-rush@users.noreply.github.com>
2021-09-06 09:55:25 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
   devise_for :users
-  root to: "plans#index"
+  #root to: "plans#index"
+	root to: "tweets#index"
 
 	resources :plans, only: :index
 end
