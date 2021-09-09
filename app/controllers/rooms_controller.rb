d06488f01fb39da28a20dcfdaf06a019afee3a44
keisuzuk-rush <84221784+keisuzuk-rush@users.noreply.github.com>
2021-09-09 11:20:45 +0900

    Rubocopで修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,30 +1,29 @@
 class RoomsController < ApplicationController
+  def index
+  end
 
-	def index
-	end
-	
-	def new
-		@room = Room.new
-	end
+  def new
+    @room = Room.new
+  end
 
-	def create	
-		@room = Room.new(room_params)
-		if @room.save
-			redirect_to rooms_path
-		else
-			render :new
-		end
-	end
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
 
-	def destroy
-		room = Room.find(params[:id])
-		room.destroy
-		redirect_to rooms_path
-	end
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to rooms_path
+  end
 
-	private
+  private
 
-	def room_params
-		params.require(:room).permit(:name, user_ids: [])
-	end
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,11 +1,11 @@
 class Tweet < ApplicationRecord
-	belongs_to :user
-	belongs_to :room
-	has_one_attached :image
+  belongs_to :user
+  belongs_to :room
+  has_one_attached :image
 
-	validates :text, presence: true, unless: :was_attached?
+  validates :text, presence: true, unless: :was_attached?
 
-	def was_attached?
-		self.image.attached?
-	end
+  def was_attached?
+    image.attached?
+  end
 end
